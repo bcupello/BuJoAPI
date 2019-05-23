1aa56f5a6e7a6ed1908fccc59a299efc3906236e
bcupello <brunelli.cupello@live.jp>
2019-05-23 12:50:17 -0300

    Changes run script

--- run_script.js
+++ run_script.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process');
-exec('sudo -S nodemon --exec babel-node src/index.js', (err, stdout, stderr) => {
+exec('echo LabProg1 | sudo -S nodemon --exec babel-node src/index.js', (err, stdout, stderr) => {
 	console.log('teste');
   if (err) {
   	console.log('teste2', err);
